1ad75f5198f7e9ab60ab82664e5fcbab96ddf5e5
Tushar Ameria <tusharameria@gmail.com>
2025-09-20 13:12:33 +0530

    recursive search

--- README.md
+++ README.md
@@ -5,9 +5,12 @@
 # Flag
 
 ```go
-go run cmd/main.go [-i] [searchQuery] [files...]
+go run cmd/main.go [-i] [-r] [-tl] [-tf] [searchQuery] [path1 path2 ... pathn]
 ```
 
 -i : turns the search case insensetive
+-r : turns the search recursive (directories inside directories, default is 1 layer ie if you provide directory as path then it will give result based on files inside that directory only)
+-tl : returns total number of lines matched
+-tf : returns total number of files matched
 searchQuery : your search string
-[files...] : array of files in which you want to search
+[path1 path2 ... pathn] : array of paths in which you want to search
--- cmd/main.go
+++ cmd/main.go
@@ -5,50 +5,123 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"path/filepath"
 	"strings"
 )
 
 func main() {
 	caseInsensetive := false
+	isRecursive := false
+	totalLines := false
+	totalFiles := false
 	flag.BoolVar(&caseInsensetive, "i", caseInsensetive, "True or False")
+	flag.BoolVar(&isRecursive, "r", isRecursive, "True or False")
+	flag.BoolVar(&totalLines, "tl", totalLines, "True or False")
+	flag.BoolVar(&totalFiles, "tf", totalFiles, "True or False")
 	flag.Parse()
 	args := flag.Args()
 
 	searchQuery := args[0]
-	fileNames := args[1:]
+	pathNames := args[1:]
 
-	for _, fileName := range fileNames {
-		file, err := os.Open(fileName)
+	totalNoLines := 0
+	totalNoFiles := 0
+
+	var recursive func(string, bool)
+	recursive = func(pathName string, firstLayer bool) {
+		isFile, err := IsFile(pathName)
 		if err != nil {
 			fmt.Printf("%s\n", err)
 			os.Exit(1)
 		}
-		defer file.Close()
 
-		scanner := bufio.NewScanner(file)
-
-		n := 1
-		for scanner.Scan() {
-			text := scanner.Text()
-			textTest := text
-			if caseInsensetive {
-				textTest = strings.ToLower(text)
-				searchQuery = strings.ToLower(searchQuery)
+		if isFile {
+			file, err := os.Open(pathName)
+			if err != nil {
+				fmt.Printf("%s\n", err)
+				os.Exit(1)
 			}
-			if strings.Contains(textTest, searchQuery) {
-				fmt.Printf("%s:%d %s\n", fileName, n, text)
+			defer file.Close()
+
+			scanner := bufio.NewScanner(file)
+
+			n := 1
+			found := false
+			for scanner.Scan() {
+				text := scanner.Text()
+				textTest := text
+				if caseInsensetive {
+					textTest = strings.ToLower(text)
+					searchQuery = strings.ToLower(searchQuery)
+				}
+				if strings.Contains(textTest, searchQuery) {
+					fmt.Printf("%s:%d %s\n", pathName, n, text)
+					if totalLines {
+						totalNoLines++
+					}
+					found = true
+				}
+				n++
 			}
-			n++
-		}
+			if found && totalFiles {
+				totalNoFiles++
+			}
 
-		if err := scanner.Err(); err != nil {
-			fmt.Printf("%s\n", err)
-			os.Exit(1)
-		}
+			if err := scanner.Err(); err != nil {
+				fmt.Printf("%s\n", err)
+				os.Exit(1)
+			}
 
-		if err := file.Close(); err != nil {
-			fmt.Printf("%s\n", err)
-			os.Exit(1)
+			if err := file.Close(); err != nil {
+				fmt.Printf("%s\n", err)
+				os.Exit(1)
+			}
+		} else {
+			if firstLayer || isRecursive {
+				isDir, err := IsDir(pathName)
+				if err != nil {
+					fmt.Printf("%s\n", err)
+					os.Exit(1)
+				}
+
+				if isDir {
+					newPaths, err := os.ReadDir(pathName)
+					if err != nil {
+						fmt.Printf("%s\n", err)
+						os.Exit(1)
+					}
+					for _, newPath := range newPaths {
+						recursive(filepath.Join(pathName, newPath.Name()), false)
+					}
+				}
+			}
 		}
 	}
+
+	for _, pathName := range pathNames {
+		recursive(pathName, true)
+	}
+
+	if totalLines {
+		fmt.Printf("Total number of lines matched : %d\n", totalNoLines)
+	}
+	if totalFiles {
+		fmt.Printf("Total number of files matched : %d\n", totalNoFiles)
+	}
+}
+
+func IsDir(path string) (bool, error) {
+	info, err := os.Stat(path)
+	if err != nil {
+		return false, err
+	}
+	return info.IsDir(), nil
+}
+
+func IsFile(path string) (bool, error) {
+	info, err := os.Stat(path)
+	if err != nil {
+		return false, err
+	}
+	return info.Mode().IsRegular(), nil
 }
