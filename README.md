0c46462bd0aa2152cd33046d4c22415d49ad2da3
Tushar Ameria <tusharameria@gmail.com>
2025-09-20 10:51:43 +0530

    multiple file search

--- README.md
+++ README.md
@@ -5,5 +5,9 @@
 # Flag
 
 ```go
-go run cmd/main.go -age [int] -name [string]
+go run cmd/main.go [-i] [searchQuery] [files...]
 ```
+
+-i : turns the search case insensetive
+searchQuery : your search string
+[files...] : array of files in which you want to search
--- cmd/main.go
+++ cmd/main.go
@@ -9,35 +9,46 @@ import (
 )
 
 func main() {
-	fileName := ""
 	caseInsensetive := false
 	flag.BoolVar(&caseInsensetive, "i", caseInsensetive, "True or False")
-	flag.StringVar(&fileName, "f", fileName, "Need valid file address")
 	flag.Parse()
 	args := flag.Args()
 
 	searchQuery := args[0]
+	fileNames := args[1:]
 
-	file, err := os.Open(fileName)
-	if err != nil {
-		fmt.Printf("%s\n", err)
-		os.Exit(1)
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-
-	i := 1
-	for scanner.Scan() {
-		text := scanner.Text()
-		textTest := text
-		if caseInsensetive {
-			textTest = strings.ToLower(text)
-			searchQuery = strings.ToLower(searchQuery)
+	for _, fileName := range fileNames {
+		file, err := os.Open(fileName)
+		if err != nil {
+			fmt.Printf("%s\n", err)
+			os.Exit(1)
 		}
-		if strings.Contains(textTest, searchQuery) {
-			fmt.Printf("%d %s\n", i, text)
+		defer file.Close()
+
+		scanner := bufio.NewScanner(file)
+
+		n := 1
+		for scanner.Scan() {
+			text := scanner.Text()
+			textTest := text
+			if caseInsensetive {
+				textTest = strings.ToLower(text)
+				searchQuery = strings.ToLower(searchQuery)
+			}
+			if strings.Contains(textTest, searchQuery) {
+				fmt.Printf("%s %d %s\n", fileName, n, text)
+			}
+			n++
 		}
-		i++
+
+		if err := scanner.Err(); err != nil {
+			fmt.Printf("%s\n", err)
+			os.Exit(1)
+		}
+
+		if err := file.Close(); err != nil {
+			fmt.Printf("%s\n", err)
+			os.Exit(1)
+		}
 	}
 }
